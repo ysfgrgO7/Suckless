7b2d000f4c9b0a2433a468d290d967cb7ed4a687
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-07-22 16:31:02 +0200

    New dwm rice

--- dwm/config.h
+++ dwm/config.h
@@ -8,16 +8,16 @@ static const unsigned int gappiv    = 20;       /* vert inner gap between window
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int vertpad            = 20;       /* vertical padding of bar */
-static const int sidepad            = 20;       /* horizontal padding of bar */
-static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 12;        /* vertical padding for statusbar */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "JetbrainsMono nerd font:size=12",
 "Material Design Icons Desktop:size=12"
 };
-// Set theme
 static const char col_bg[]          = "#1E1D2F";
 static const char col_gray[]        = "#6E6C7E";
 static const char col_white[]       = "#D9E0EE";
@@ -33,10 +33,12 @@ static const char *colors[][3]      = {
 };
 
 static const char *const autostart[] = {
+	"volumeicon", NULL,
+	"picom", NULL,
+	"lxsession", NULL,
     "slstatus", NULL,
-	"nitrogen", "--restore", NULL, // wallpapers
-	"picom", "-b", "--experimental-backends", NULL, // compositer
-	NULL // terminate
+	"picom", "-b", "--experimental-backends", NULL,
+	NULL
 };
 
 typedef struct {
@@ -54,7 +56,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 " };
+static const char *tags[] = { " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 " };
 static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
 static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
 static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
@@ -69,7 +71,6 @@ static const Rule rules[] = {
 	{ NULL,		  "spmus",	  NULL,		SPTAG(2),		1,			 -1 },
 };
 
-/* layout(s) */
 static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
@@ -79,7 +80,8 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 #include "vanitygaps.c"
 
 static const Layout layouts[] = {
- 	{ "[]=",      tile },    /* first entry is default */
+	/* symbol     arrange function */
+	{ "[]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
 	{ "[@]",      spiral },
 	{ "[\\]",     dwindle },
@@ -118,6 +120,7 @@ static const char *tuifmcmd[]     = { "st", "-e", "ranger", NULL };
 static const char *webcmd[]       = { "brave", NULL };
 static const char *scrotcmd[]     = { "scrot", NULL };
 
+
 static Key keys[] = {
 // Apps 
 { MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
@@ -130,11 +133,11 @@ static Key keys[] = {
 { MODKEY|ShiftMask,             XK_e,      spawn,          {.v = fmcmd } },
 // Layout Controls
 { MODKEY,                       XK_f,      togglefloating, {0} },
-{ MODKEY,                       XK_Tab,    cyclelayout,    {.i = +1 } }, 
+{ MODKEY,                       XK_Tab,    cyclelayout,    {.i = +1 } },
 { MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 { MODKEY|ShiftMask,             XK_space,  togglefullscr,  {0} },
-{ MODKEY,                       XK_b,  togglebar,      {0} },
-{ MODKEY,                       XK_c, zoom,           {0} },
+{ MODKEY,                       XK_b,      togglebar,      {0} },
+{ MODKEY,                       XK_c,      zoom,           {0} },
 // Dwm Stuff
 { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
@@ -149,7 +152,7 @@ static Key keys[] = {
 { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
 //Scratchpads
-{ MODKEY,                       XK_p,      togglescratch,  {.ui = 0 } },
+{ MODKEY,                       XK_p,        togglescratch,  {.ui = 0 } },
 { MODKEY,                       XK_s,	     togglescratch,  {.ui = 1 } },
 { MODKEY,                       XK_m,	     togglescratch,  {.ui = 2 } },
 // Tags
@@ -183,12 +186,14 @@ TAGKEYS(                        XK_8,                      7)
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	/* click                event mask      button          function        argument */
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
-	{ ClkClientWin,         MODKEY,         Button3,        resizemouse,    {0} },
+	{ ClkClientWin,         MODKEY,         Button1,        resizemouse,    {0} },
 	{ ClkTagBar,            0,              Button1,        view,           {0} },
 	{ ClkTagBar,            0,              Button3,        toggleview,     {0} },
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },

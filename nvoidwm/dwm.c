5937c80f65fda7cd6ab6862437179f753d5042d4
ysfgrgO7 <ysfgrg2007@gmail.com>
2022-09-25 21:40:18 +0200

    adding cursor warp

--- nvoidwm/dwm.c
+++ nvoidwm/dwm.c
@@ -349,6 +349,7 @@ static void updatetitle(Client *c);
 static void updatewindowtype(Client *c);
 static void updatewmhints(Client *c);
 static void view(const Arg *arg);
+static void warp(const Client *c);
 static Client *wintoclient(Window w);
 static Monitor *wintomon(Window w);
 static Client *wintosystrayicon(Window w);
@@ -1721,6 +1722,7 @@ void focusmon(const Arg *arg) {
   unfocus(selmon->sel, 0);
   selmon = m;
   focus(NULL);
+  warp(selmon->sel);
 }
 
 void focusstack(const Arg *arg) {
@@ -2568,8 +2570,9 @@ void restack(Monitor *m) {
       }
   }
   XSync(dpy, False);
-  while (XCheckMaskEvent(dpy, EnterWindowMask, &ev))
-    ;
+  while (XCheckMaskEvent(dpy, EnterWindowMask, &ev));
+  if (m == selmon && (m->tagset[m->seltags] & m->sel->tags) && selmon->lt[selmon->sellt] != &layouts[2])
+      warp(m->sel);
 }
 
 void run(void) {
@@ -3633,6 +3636,26 @@ void view(const Arg *arg) {
   focus(NULL);
   arrange(selmon);
   	updatecurrentdesktop();
+}
+
+void warp(const Client *c){
+	int x, y;
+
+	if (!c) {
+		XWarpPointer(dpy, None, root, 0, 0, 0, 0, selmon->wx + selmon->ww/2, selmon->wy + selmon->wh/2);
+		return;
+	}
+
+	if (!getrootptr(&x, &y) ||
+	    (x > c->x - c->bw &&
+	     y > c->y - c->bw &&
+	     x < c->x + c->w + c->bw*2 &&
+	     y < c->y + c->h + c->bw*2) ||
+	    (y > c->mon->by && y < c->mon->by + bh) ||
+	    (c->mon->topbar && !y))
+		return;
+
+	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w / 2, c->h / 2);
 }
 
 Client *wintoclient(Window w) {
